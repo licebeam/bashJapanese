bfa28551c2c9476f446708f4dae50b4dcb292162
licebeam <dustinwalkerart@gmail.com>
2018-09-18 18:51:47 +0900

    added two new words

--- japanese/japanese100.js
+++ japanese/japanese100.js
@@ -1,13 +1,16 @@
 module.exports = [
   {
+    id: 1,
     question: `行く | いく | Iku`,
     answer: 'to go'
   },
   {
+    id: 2,
     question: `見る | みる | Miru`,
     answer: 'to see'
   },
   {
+    id: 3,
     question: `多い | おおい | Ooi`,
     answer: 'many'
   },
@@ -16,7 +19,21 @@ module.exports = [
     answer: 'home'
   },
   {
+    num: 4,
     question: `これ | Kore`,
     answer: 'this'
   }
+  ,
+  {
+    num: 5,
+    question: `それ | Sore`,
+    answer: 'that'
+  }
+  ,
+  {
+    num: 6,
+    question: `私 | わたし | Kore`,
+    answer: 'i'
+  }
+
 ]
