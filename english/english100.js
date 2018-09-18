9d2a472df8ea18ce39ee4e23063dc94d4eb9053a
licebeam <dustinwalkerart@gmail.com>
2018-09-18 18:59:31 +0900

    added words for english

--- english/english100.js
+++ english/english100.js
@@ -1,6 +1,38 @@
 module.exports = [
   {
-    question: `To go`,
-    answer: 'いく'
+    id: 1,
+    answer: `いく`,
+    question: 'To go'
   },
+  {
+    id: 2,
+    answer: `みる`,
+    question: 'To see'
+  },
+  {
+    id: 3,
+    answer: `おおい`,
+    question: 'Many'
+  },
+  {
+    answer: `うち`,
+    question: 'Home'
+  },
+  {
+    num: 4,
+    answer: `これ`,
+    question: 'This'
+  }
+  ,
+  {
+    num: 5,
+    answer: `それ`,
+    question: 'That'
+  }
+  ,
+  {
+    num: 6,
+    answer: `わたし`,
+    question: 'I, myself'
+  }
 ]
